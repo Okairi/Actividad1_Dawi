976d9b70ac6f6ee841a7a2ac52f2c6e7103ca095
Alessandro <alessandroRetuerto@hotmail.com>
2021-08-30 19:51:53 -0500

    Registrar y actualizar tabla medicamento

---
+++ src/main/java/com/empresa/service/AlumnoService.java
@@ -0,0 +1,20 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import com.empresa.entity.Alumno;
+
+ 
+
+public interface AlumnoService {
+
+	
+	
+	public Alumno insertaActualiza(Alumno obj);
+	public List<Alumno> listaAlumno();
+	
+	
+	
+	
+	
+}
---
+++ src/main/java/com/empresa/service/AlumnoServiceImpl.java
@@ -0,0 +1,35 @@
+package com.empresa.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import com.empresa.entity.Alumno;
+import com.empresa.repository.AlumnoRepository;
+
+@Service
+public class AlumnoServiceImpl implements AlumnoService{
+
+	@Autowired
+	private AlumnoRepository repository;
+	
+	
+	
+	@Override
+	public Alumno insertaActualiza(Alumno obj) {
+		// TODO Auto-generated method stub
+		return repository.save(obj);
+	}
+
+	@Override
+	public List<Alumno> listaAlumno() {
+		// TODO Auto-generated method stub
+		return repository.findAll();
+	}
+
+	
+	
+	
+}
